123dfbc993b59a8ce44b41cee3197fe73d038a26
kokomme <116422742+KomedaMasaya@users.noreply.github.com>
2025-12-08 22:25:44 +0900

    feat: ContentAreaコンポーネントに編集ボタンを追加し、レイアウトを改善

--- src/features/content/components/ContentArea.tsx
+++ src/features/content/components/ContentArea.tsx
@@ -1,17 +1,34 @@
+
 import React from 'react';
 import Title from './title/Title';
 import TextArea from './text-area/TextArea';
+import EditButton from './edit-button/EditButton';
 import './ContentArea.css';
 
 const ContentArea: React.FC = () => {
+	const handleStartEdit = () => {
+		console.log('Edit clicked');
+	};
+
 	return (
 		<section className="content-area">
-			<Title>坊ちゃん</Title>
-			<TextArea>
-				<p>
-					親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。
-				</p>
-			</TextArea>
+			<div className="content-header">
+				<Title>坊ちゃん</Title>
+				<div className="header-actions">
+					<EditButton onClick={handleStartEdit} />
+				</div>
+			</div>
+
+			<div className="textarea-wrapper">
+				<TextArea>
+					<p>
+						親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。
+					</p>
+				</TextArea>
+				<div className="textarea-actions">
+					<EditButton onClick={handleStartEdit} />
+				</div>
+			</div>
 		</section>
 	);
 };
