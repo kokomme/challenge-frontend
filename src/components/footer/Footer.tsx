64c6c838aa06433511b61be2a9794443e3a4cdf6
kokomme <116422742+KomedaMasaya@users.noreply.github.com>
2025-12-08 23:40:56 +0900

    feat: フッターコンポーネントを追加し、レイアウトを整備

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="app-footer">
+      <div className="footer-left">Copyright © 2021 Sample</div>
+      <div className="footer-right">運営会社</div>
+    </footer>
+  );
+};
+
+export default Footer;
