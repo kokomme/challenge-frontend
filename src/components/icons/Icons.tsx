846357e0a6bd91d55e6d5b6c96592fe090e5198e
kokomme <116422742+KomedaMasaya@users.noreply.github.com>
2025-12-08 20:36:53 +0900

    feat: vite-plugin-svgrを追加し、アイコンコンポーネントをSVGとしてインポートするように変更

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -1,47 +1,42 @@
-import saveSvg from '../../assets/icon/save.svg?raw';
-import editSvg from '../../assets/icon/edit.svg?raw';
-import doneSvg from '../../assets/icon/done.svg?raw';
-import cancelSvg from '../../assets/icon/cancel.svg?raw';
-import logoSvg from '../../assets/icon/logo.svg?raw';
-import plusSvg from '../../assets/icon/plus.svg?raw';
-import deleteSvg from '../../assets/icon/delete.svg?raw';
+import SaveSvg from '../../assets/icon/save.svg?react';
+import EditSvg from '../../assets/icon/edit.svg?react';
+import DoneSvg from '../../assets/icon/done.svg?react';
+import CancelSvg from '../../assets/icon/cancel.svg?react';
+import LogoSvg from '../../assets/icon/logo.svg?react';
+import PlusSvg from '../../assets/icon/plus.svg?react';
+import DeleteSvg from '../../assets/icon/delete.svg?react';
 
 type IconProps = {
   className?: string;
   'aria-hidden'?: boolean;
 };
 
-function RawSvg({ svg, className = '', ['aria-hidden']: ariaHidden = true }: { svg: string } & IconProps) {
-  const injected = svg.replace('<svg', `<svg class="${className}" aria-hidden="${ariaHidden}"`);
-  return <span dangerouslySetInnerHTML={{ __html: injected }} />;
+export function SaveIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <SaveSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function SaveIcon(props: IconProps) {
-  return <RawSvg svg={saveSvg} {...props} />;
+export function EditIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <EditSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function EditIcon(props: IconProps) {
-  return <RawSvg svg={editSvg} {...props} />;
+export function DoneIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <DoneSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function DoneIcon(props: IconProps) {
-  return <RawSvg svg={doneSvg} {...props} />;
+export function CancelIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <CancelSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function CancelIcon(props: IconProps) {
-  return <RawSvg svg={cancelSvg} {...props} />;
+export function LogoIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <LogoSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function LogoIcon(props: IconProps) {
-  return <RawSvg svg={logoSvg} {...props} />;
+export function PlusIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <PlusSvg className={className} aria-hidden={ariaHidden} />;
 }
 
-export function PlusIcon(props: IconProps) {
-  return <RawSvg svg={plusSvg} {...props} />;
-}
-
-export function DeleteIcon(props: IconProps) {
-  return <RawSvg svg={deleteSvg} {...props} />;
+export function DeleteIcon({ className = '', 'aria-hidden': ariaHidden = true }: IconProps) {
+  return <DeleteSvg className={className} aria-hidden={ariaHidden} />;
 }
 
 export default {
